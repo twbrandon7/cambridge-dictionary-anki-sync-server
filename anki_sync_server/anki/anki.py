0882b07858675a845afc4949aeb209b909a6a5bc
Li-Xian Chen <twbrandon7@gmail.com>
2024-06-02 15:57:14 +0800

    refactor: remove username and password from anki service

--- anki_sync_server/anki/anki.py
+++ anki_sync_server/anki/anki.py
@@ -1,3 +1,4 @@
+import copy
 import time
 from threading import Lock
 
@@ -6,7 +7,7 @@ from anki.notes import Note
 
 from anki_sync_server.anki.media_creator import MediaCreator
 from anki_sync_server.anki.model_creator import ModelCreator
-from anki_sync_server.thread import ThreadWithReturnValue
+from anki_sync_server.setup.credential_storage import CredentialStorage
 from anki_sync_server.tts.base import TtsService
 from anki_sync_server.utils import remove_anki_cloze_tags, remove_html_tags
 
@@ -15,8 +16,6 @@ class Anki:
     def __init__(
         self,
         collection: Collection,
-        username: str,
-        password: str,
         tts_service: TtsService,
         deck_name: str = "English Vocabulary",
         media_sync_timeout_seconds: int = 600,
@@ -27,19 +26,8 @@ class Anki:
         self._model_creator = ModelCreator(collection)
         self._media_creator = MediaCreator(collection)
         self._anki_model = None
-        self._username = username
-        self._password = password
         self._tts_service = tts_service
-        sync_thread = ThreadWithReturnValue(
-            target=collection.sync_login,
-            args=(
-                username,
-                password,
-                "https://sync.ankiweb.net",
-            ),
-        )
-        sync_thread.start()
-        self._auth = sync_thread.join()
+        self._auth = CredentialStorage().get_anki_session()
         self._media_sync_timeout_seconds = media_sync_timeout_seconds
 
     def add_cloze_note(
@@ -95,9 +83,8 @@ class Anki:
         sync_attempt_result = self._collection.sync_collection(self._auth, True)
 
         if sync_attempt_result.new_endpoint != "":
-            new_auth = self._collection.sync_login(
-                self._username, self._password, sync_attempt_result.new_endpoint
-            )
+            new_auth = copy.deepcopy(self._auth)
+            new_auth.endpoint = sync_attempt_result.new_endpoint
         else:
             new_auth = self._auth
 
